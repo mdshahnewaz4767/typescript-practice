4ba6bdace05a8adb1ca1497da19f74ecc47a8f31
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-12 17:47:00 +0600

    Set function parameter type and function return type

--- first.ts
+++ first.ts
@@ -4,4 +4,23 @@ let club: string = 'Paris';
 const isFamous: boolean = false;
 let famous: boolean;
 console.log(isFamous);
+
 ////==End number, string, boolean==//
+
+
+//===function parameter type==//
+function add(num1: number | string, num2: number | string){
+    return num1 + num2;
+}
+add(3, 40);
+add('mun', 'newaz');
+add(4, 'Someone');
+
+//another
+function fullName(FirstName: string, LastName: string): string{
+    return FirstName + ' ' + LastName;
+}
+const user: string = fullName('Muhammad', 'Shahnewaz');
+console.log(user);
+
+//==End function parameter type==//
